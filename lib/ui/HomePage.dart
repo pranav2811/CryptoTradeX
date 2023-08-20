601e8ab9be3391e54e321333cb1a466613a7b0d4
Pranav Powar <powar.pranav29@gmail.com>
2023-08-20 17:57:05 +0530

    clickable bottomnavbar

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:cryptotradex/ui/watchlist.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -11,50 +12,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   int _selectedIndex = 0;
 
-  static const List<Widget> _widgetOptions = <Widget>[
-    
-  ];
+  static const List<Widget> _widgetOptions = <Widget>[Watchlist()];
   void onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
-  @override
-  void initState() {
-    super.initState();
-  }
 
-  @override
-  void dispose(){
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark_border),
-              label: 'Watchlist',
-              backgroundColor: Colors.blue,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.suitcase),
-              label: 'Portfolio',
-              backgroundColor: Colors.blue,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline),
-              label: 'Profile',
-              backgroundColor: Colors.blue,
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: onItemTapped,
-        ),
-      ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark_border),
+                label: 'Watchlist',
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.suitcase),
+                label: 'Portfolio',
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                label: 'Profile',
+                backgroundColor: Colors.blue,
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: onItemTapped,
+          ),
+          body: Container(
+            color: const Color(0xFFEEEEEE),
+            child: Column(children: <Widget>[
+              Expanded(child: _widgetOptions.elementAt(_selectedIndex)),
+            ]),
+          )),
     );
   }
 }
